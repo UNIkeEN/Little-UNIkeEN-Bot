453f9d2ef79e5591c65536c39b19699b407e73a0
ubuntu <ubuntu@localhost.localdomain>
2023-08-02 02:48:59 +0800

    update mua

---
+++ plugins/mua/common/packets.py
@@ -0,0 +1,165 @@
+import json
+
+class Packet:
+    TYPE = None
+
+    def __init__(self):
+        pass
+
+    def serialize_content() -> str:
+        pass
+
+    def __str__(self) -> str:
+        return self.to_json()
+
+    def to_json(self) -> str:
+        content = self.serialize_content()
+        return json.dumps({"type" : self.TYPE, "content" : content}, ensure_ascii=False)
+    
+    @staticmethod
+    def from_json(src):
+        obj = json.loads(src)
+        packet_type = REGISTRY[obj["type"]]
+        return packet_type.from_json_content(obj["content"])
+    
+    @staticmethod
+    def from_json_content():
+        raise NotImplementedError()
+    
+class ClientAuthPacket(Packet):
+    TYPE = "CLIENT_AUTH"
+
+    AUTH_DEFAULT = "DEFAULT"
+
+    def __init__(self, auth_type, auth_field):
+        self.auth_type = auth_type
+        self.auth_field = auth_field
+
+    @classmethod
+    def default_auth_type(cls, id, token):
+        return cls(cls.AUTH_DEFAULT, {"id" : id,  "token" : token})
+    
+    def serialize_content(self):
+        return {"type" : self.auth_type, "field" : self.auth_field}
+
+    @classmethod
+    def from_json_content(cls, content : dict):
+        return cls(content["type"], content["field"])
+    
+class AuthSuccessPacket(Packet):
+    TYPE = "AUTH_SUCCESS"
+
+    def __init__(self, id):
+        self.id = id
+    
+    def serialize_content(self):
+        return {}
+
+    @classmethod
+    def from_json_content(cls, content : dict):
+        return cls(content["id"])
+    
+    def serialize_content(self):
+        return {"id" : self.id}
+
+class ErrorPacket(Packet):
+    TYPE = "ERROR"
+
+    E_AUTH_FAILURE = "AUTH_FAILURE"
+    E_UNKNOWN_TARGET = "UNKNOWN_TARGET"
+
+    def __init__(self, error_code, error_info = None):
+        self.error_code = error_code
+        self.error_info = error_info
+    
+    def to_json(self):
+        self.content = {"code" : self.error_code, "info" : self.error_info}
+
+    @classmethod
+    def from_json_content(cls, content : dict):
+        return cls(content["code"], content.get("info", None))
+
+class PayloadPacket(Packet):
+    TYPE = "PAYLOAD"
+    SUBPROTOCOL_TYPE = None
+
+    __SUBPROTOCOL_REGISTRY = {}
+
+    def __init__(self, subprotocol_name : str, subprotocol_version : int, subprotocol_packet_type : str, body : str = None, targets : list = None, sender : str = None, session_id = None):
+        self.subprotocol_name : str = subprotocol_name
+        self.subprotocol_version : int = subprotocol_version
+        self.subprotocol_packet_type : str = subprotocol_packet_type
+        self.targets : list = targets
+        self.body : str = body
+        self.sender : str = sender
+        self.session_id = session_id
+
+    def get_json_body(self) -> str:
+        return self.body
+
+    def get_subprotocol_name(self) -> str:
+        return self.subprotocol_name
+    
+    def get_subprotocol_version(self) -> int:
+        return self.subprotocol_version
+    
+    def get_subprotocol_packet_type(self) -> str:
+        return self.subprotocol_packet_type
+    
+    def serialize_content(self) -> str:
+        content = { 
+            "subprotocol" : {
+                "name" : self.subprotocol_name,
+                "version" : self.subprotocol_version,
+                "packet_type" : self.subprotocol_packet_type
+            },
+            "body" : self.get_json_body(),
+        }
+        if self.targets is not None:
+            content["targets"] = self.targets
+        if self.session_id is not None:
+            content["session_id"] = self.session_id
+        return content
+        
+    def get_session_id(self):
+        return self.session_id
+        
+    def set_session_id(self, session_id):
+        self.session_id = session_id
+
+    def set_body(self, body):
+        self.body = body
+
+    def set_target(self, target : list):
+        self.targets = target
+
+    @classmethod
+    def from_payload_packet(cls, payload_packet):
+        raise NotImplementedError
+
+    @classmethod
+    def from_json_content(cls, content : dict):
+        return cls(
+            content["subprotocol"]["name"], 
+            content["subprotocol"]["version"], 
+            content["subprotocol"]["packet_type"], 
+            content.get("body", None), 
+            content.get("targets", None), 
+            session_id=content.get("session_id", None)
+        )
+
+    def as_subprotocol_packet(self):
+        if self.subprotocol_name in self.__SUBPROTOCOL_REGISTRY and self.subprotocol_packet_type in self.__SUBPROTOCOL_REGISTRY[self.subprotocol_name]:
+            return self.__SUBPROTOCOL_REGISTRY[self.subprotocol_name][self.subprotocol_packet_type].from_payload_packet(self)
+        return None
+    
+    @classmethod
+    def register_subprotocol(cls, subprotocol_name, subprotocol_registry):
+        cls.__SUBPROTOCOL_REGISTRY[subprotocol_name] = subprotocol_registry
+
+REGISTRY = {
+    ClientAuthPacket.TYPE : ClientAuthPacket,
+    AuthSuccessPacket.TYPE : AuthSuccessPacket,
+    ErrorPacket.TYPE : ErrorPacket,
+    PayloadPacket.TYPE : PayloadPacket,
+}
---
+++ plugins/mua/common/subprotocols.py
@@ -0,0 +1,190 @@
+from .packets import PayloadPacket
+import time
+from typing import Union, Optional, List, Tuple, Set
+# --------------------
+#     ANNOUNCEMENT
+# --------------------
+
+SUBPROTOCOL_ANNOUNCEMENT_NAME = "ANNOUNCEMENT"
+SUBPROTOCOL_ANNOUNCEMENT_VERSION = 1
+
+class Announcement:
+    def __init__(self, 
+                title : str, 
+                content,
+                author_token : Optional[str],
+                channel : str,
+                tags : Union[List[str], Tuple[str], Set[str], None],
+                targets : Union[List[str], Tuple[str], Set[str], None],
+                time_created : int,
+                time_expires : Optional[int],
+                meta : dict = {}):
+        self.title = title
+        self.content = content
+        self.author_token = author_token
+        self.author_id = None
+        self.channel = channel
+        self.tags = tags
+        self.targets = targets
+        self.time_created = time_created
+        self.time_expires = time_expires
+        self.meta = meta
+
+    def serialize(self, is_server=False):
+        r = {
+            "title" : self.title,
+            "content" : self.content,
+            "channel" : self.channel,
+            "tags" : self.tags,
+            "time_created" : self.time_created,
+            "meta" : self.meta
+        }
+        if not is_server:
+            r["author_token"] = self.author_token
+        if self.author_id is not None:
+            r["author_id"] = self.author_id
+        if self.time_expires is not None:
+            r["time_expires"] = self.time_expires
+        if self.targets is not None:
+            r["time_expires"] = self.targets
+        return r
+    
+    @classmethod
+    def from_json(cls, s):
+        ret = cls(
+            s["title"],
+            s["content"],
+            s.get("author_token", None),
+            s["channel"],
+            s["tags"],
+            s.get("targets", None),
+            s["time_created"],
+            s.get("time_expires", None),
+            s["meta"]
+        )
+        ret.author_id = s.get("author_id", None)
+        return ret
+
+    def __hash__(self) -> int:
+        return hash(self.title + self.content + self.channel)
+
+class CreateAnnouncementPacket(PayloadPacket):
+    SUBPROTOCOL_TYPE = "CREATE"
+
+    def __init__(self, announcement : Announcement, is_server=False, session_id=None):
+        super().__init__(
+            subprotocol_name = SUBPROTOCOL_ANNOUNCEMENT_NAME,
+            subprotocol_version = SUBPROTOCOL_ANNOUNCEMENT_VERSION, 
+            subprotocol_packet_type = self.SUBPROTOCOL_TYPE,
+            session_id=session_id
+        )
+        self.announcement = announcement
+        self.is_server = is_server
+
+    def get_json_body(self):
+        return self.announcement.serialize(self.is_server)
+
+    @classmethod
+    def from_payload_packet(cls, payload : PayloadPacket):
+        return cls(
+            Announcement.from_json(payload.body)
+        )
+
+class AnnouncementOperationResultPacket(PayloadPacket):
+    SUBPROTOCOL_TYPE = "RESULT"
+
+    def __init__(self, success : bool, aid : int, reason : str = None):
+        super().__init__(
+            subprotocol_name = SUBPROTOCOL_ANNOUNCEMENT_NAME,
+            subprotocol_version = SUBPROTOCOL_ANNOUNCEMENT_VERSION, 
+            subprotocol_packet_type = self.SUBPROTOCOL_TYPE,
+        )
+        self.success = success
+        self.reason = reason
+        self.aid = aid
+        self.body = {
+            "success" : success,
+            "reason" : reason,
+            "aid" : aid
+        }
+
+    @classmethod
+    def from_payload_packet(cls, payload : PayloadPacket):
+        return cls(
+            payload.body["success"],
+            payload.body.get("reason", None),
+            payload.body["id"]
+        )
+
+class DeleteAnnouncementPacket(PayloadPacket):
+    SUBPROTOCOL_TYPE = "DELETE"
+    
+    def __init__(self, aid : int, author_token : str):
+        super().__init__(
+            subprotocol_name = SUBPROTOCOL_ANNOUNCEMENT_NAME,
+            subprotocol_version = SUBPROTOCOL_ANNOUNCEMENT_VERSION, 
+            subprotocol_packet_type = self.SUBPROTOCOL_TYPE,
+        )
+        self.aid = aid
+        self.author_token = author_token
+        self.body = {
+            "aid" : aid,
+            "author_token" : author_token
+        }
+
+    @classmethod
+    def from_payload_packet(cls, payload : PayloadPacket):
+        return cls(payload.body["aid"], payload.body["author_token"])
+
+class QueryAnnouncementListPacket(PayloadPacket):
+    SUBPROTOCOL_TYPE = "QUERY"
+    
+    def __init__(self):
+        super().__init__(
+            subprotocol_name = SUBPROTOCOL_ANNOUNCEMENT_NAME,
+            subprotocol_version = SUBPROTOCOL_ANNOUNCEMENT_VERSION, 
+            subprotocol_packet_type = self.SUBPROTOCOL_TYPE,
+        )
+
+    @classmethod
+    def from_payload_packet(cls, payload : PayloadPacket):
+        return cls()
+
+class AnnouncementListPacket(PayloadPacket):
+    SUBPROTOCOL_TYPE = "LIST"
+    
+    def __init__(self, announcement_list : dict):
+        super().__init__(
+            subprotocol_name = SUBPROTOCOL_ANNOUNCEMENT_NAME,
+            subprotocol_version = SUBPROTOCOL_ANNOUNCEMENT_VERSION, 
+            subprotocol_packet_type = self.SUBPROTOCOL_TYPE,
+        )
+        self.announcement_list = announcement_list
+
+    def get_json_body(self):
+        body = {}
+        for channel in self.announcement_list:
+            body[channel] = []
+            for announcement in self.announcement_list[channel]:
+                body[channel].append(announcement)
+        return body
+
+    @classmethod
+    def from_payload_packet(cls, payload : PayloadPacket):
+        announcement_list = {}
+        for channel in payload.body:
+            announcement_list[channel] = []
+            for announcement_json in payload.body[channel]:
+                announcement_list[channel].append(Announcement.from_json(announcement_json))
+        return cls(
+            announcement_list
+        )
+
+SUBPROTOCOL_ANNOUNCEMENT_REGISTRY = {
+    CreateAnnouncementPacket.SUBPROTOCOL_TYPE : CreateAnnouncementPacket,
+    AnnouncementOperationResultPacket.SUBPROTOCOL_TYPE : AnnouncementOperationResultPacket,
+    DeleteAnnouncementPacket.SUBPROTOCOL_TYPE : DeleteAnnouncementPacket,
+    QueryAnnouncementListPacket.SUBPROTOCOL_TYPE : QueryAnnouncementListPacket,
+    AnnouncementListPacket.SUBPROTOCOL_TYPE : AnnouncementListPacket,
+}
+PayloadPacket.register_subprotocol(SUBPROTOCOL_ANNOUNCEMENT_NAME, SUBPROTOCOL_ANNOUNCEMENT_REGISTRY)
